eb9968e7ff429c8099bfecbe2fcc1a73e2e4a334
Michael Bang <michael@katnegermis.dk>
2013-11-17 23:24:33 +0100

    Fix filestube-file title because webpage changed. Also rewrite module to be more pythonic.

--- utils/downloadscrapers/filestube.py
+++ utils/downloadscrapers/filestube.py
@@ -7,21 +7,20 @@ from interface import LinkScraperInterface
 
 class Filestube(LinkScraperInterface):
     _BASE_URL = "http://www.filestube.com/"
-    _LIST_URL = _BASE_URL + "query.html?q={}&select=All&hosting=24,81&page={}&sizefrom={}&sizeto={}"
+    _LIST_URL = (_BASE_URL + "query.html?q={query}&select=All&hosting=24,81&"
+                 "page={{pageid}}&sizefrom={sizefrom}&sizeto={sizeto}")
     _LINKS_PER_PAGE = 10
 
     def getlinks(self, query, numlinks=5, size=(100, 1500)):
         query = query.replace(" ", "+")
-        linkpages = []
-        for i in range(1, ((numlinks / self._LINKS_PER_PAGE) + 1) + 1):  # important that integer division is used here.
-            url = self._LIST_URL.format(query, i, size[0], size[1])
-            linkpages += self._querygetlinkpages(url)
-        links = []
-        for linkpage in linkpages[:numlinks]:
-            links.append(self._linkpagegetlink(linkpage))
-        return links
+        url = self._LIST_URL.format(query=query, sizefrom=size[0], sizeto=size[1])
+        pageids = xrange(1, ((numlinks // self._LINKS_PER_PAGE) + 1) + 1)
+        downloadpages = []
+        for i in pageids:
+            downloadpages += self._getdownloadpagelinks(url.format(pageid=i))
+        return map(self._getfilehostlinks, downloadpages[:numlinks])
 
-    def _querygetlinkpages(self, url):
+    def _getdownloadpagelinks(self, url):
         try:
             html = requests.get(url).text
         except requests.exceptions.ConnectionError:
@@ -32,18 +31,21 @@ class Filestube(LinkScraperInterface):
         links = []
         for result in results:
             try:
-                url = self._BASE_URL + result.cssselect("a.resultsLink")[0].get('href')
-                title = result.cssselect("a.resultsLink")[0].get('title')
-                links.append(Link(title=title, uris=url))
+                a_tag = result.cssselect("a.resultsLink")[0]
+                link = self._BASE_URL + a_tag.get('href')
+                links.append(link)
             except IndexError:
                 print "there was no link on the page"
-                # there was no link on the page.
-                # this happens if the file is known to be removed from the filehost
                 continue
         return links
 
-    def _linkpagegetlink(self, link):
-        html = requests.get(link.uris[0]).text
+    def _getfilehostlinks(self, url):
+        html = requests.get(url).text
         doc = lxml.html.fromstring(html)
         links = doc.cssselect("pre#copy_paste_links")[0].text_content().strip("\"").strip().split()
-        return Link(title=link.title, uris=links)
+        title = doc.cssselect("div.dotter h1")[0].text_content()[:-9]  # -9 here to remove " download"
+        return Link(title=title, uris=links)
+
+if __name__ == '__main__':
+    filestube = Filestube()
+    print filestube.getlinks("how i met your mother s06e03")
