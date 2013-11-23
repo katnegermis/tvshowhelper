5236dee73d258748226b251aa7f86655fc67dd4b
Michael Bang <michael@katnegermis.dk>
2013-11-23 18:13:49 +0100

    Make usable in more general case by adding query function and removing getimdblink function.

--- tvshowhelper/informationscrapers/google.py
+++ tvshowhelper/informationscrapers/google.py
@@ -1,27 +1,23 @@
-import random
-
 import requests
 import lxml.html
 
 
-USER_AGENTS = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
-               'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0'
-               'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0',
-               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5',
-               'Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5',)
+from tvshowhelper.informationscrapers import random_useragent
 
 
-def gethtml(query):
+def _gethtml(query):
     query = query.replace(' ', '+')
     response = requests.get('https://www.google.com/search?ie=utf-8&q={}'.format(query),
-                            headers={'User-Agent': random.choice(USER_AGENTS)})
+                            headers={'User-Agent': random_useragent()})
     return response.text
 
 
-def getimdblink(query):
-    html = gethtml(query)
+def query(query_):
+    if not isinstance(query_, str):
+        raise TypeError("You can only use strings for queries")
+    html = _gethtml(query_)
     links = lxml.html.fromstring(html).cssselect('h3.r a')
-    if links == []:
-        return None
-    else:
-        return links[0].get('href')
+    if not links:
+        return ()
+    return tuple({'link': link.get('href'), 'title': link.text_content()}
+                 for link in links)
