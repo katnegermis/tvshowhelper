4579eb0b8d8dee9565acfc34e1f7100f779325a7
Michael Bang <michael@katnegermis.dk>
2013-12-01 17:37:32 +0100

    Add parallelization of getlinks. Can be parallelized more, and isn't very pretty.

--- tvshowhelper/downloadscrapers/filestube.py
+++ tvshowhelper/downloadscrapers/filestube.py
@@ -1,9 +1,11 @@
 import requests
 import lxml.html
+from datetime import datetime
 
 from tvshowhelper.classes.link import Link
 from interface import LinkScraperInterface
 from tvshowhelper import logger
+from tvshowhelper.parallel import parallel_map
 
 
 class Filestube(LinkScraperInterface):
@@ -13,13 +15,27 @@ class Filestube(LinkScraperInterface):
     _LINKS_PER_PAGE = 10
 
     def getlinks(self, query, numlinks=5, size=(100, 1500)):
+        start = datetime.now()
         query = query.replace(" ", "+")
         url = self._LIST_URL.format(query=query, sizefrom=size[0], sizeto=size[1])
-        pageids = xrange(1, ((numlinks // self._LINKS_PER_PAGE) + 1) + 1)
-        downloadpages = []
-        for i in pageids:
-            downloadpages += self._getdownloadpagelinks(url.format(pageid=i))
-        return map(self._getfilehostlinks, downloadpages[:numlinks])
+        links = []
+        for i in range(5):
+            downloadpages = self._getdownloadpagelinks(url.format(pageid=i))
+            tmp_links = parallel_map(self._getfilehostlinks, downloadpages)
+            links = filter(lambda x: x is not None, parallel_map(self.validate, tmp_links))
+            if len(links) >= numlinks:
+                break
+        stop = datetime.now()
+        logger.debug("getlinks took: {time}".format(time=stop - start))
+        return links[:numlinks]
+
+    def validate(self, link):
+        for uri in link.uris:
+            response = requests.get(uri)
+            logger.debug("{link}: {response}: {status}".format(link=uri, response=response.url, status=response.status_code))
+            if response.status_code != 200 or response.url[-3:] == "404":
+                return
+        return link
 
     def _getdownloadpagelinks(self, url):
         try:
@@ -46,3 +62,4 @@ class Filestube(LinkScraperInterface):
         links = doc.cssselect("pre#copy_paste_links")[0].text_content().strip("\"").strip().split()
         title = doc.cssselect("div.dotter h1")[0].text_content()[:-9]  # -9 here to remove " download"
         return Link(title=title, uris=links)
+
