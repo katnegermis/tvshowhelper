a0f2fdb8593e91c4c0c04ae5a05712888616af1c
Michael Bang <michael@katnegermis.dk>
2014-01-04 12:50:24 +0100

    Remove print statements from test.

--- tests/tvshowhelper/parallel.py
+++ tests/tvshowhelper/parallel.py
@@ -1,7 +1,6 @@
 import unittest
 from os.path import dirname, join, abspath
 import sys
-from datetime import datetime
 
 here = lambda *args: join(abspath(dirname(__file__)), *args)
 root = here("..", "..")
@@ -17,17 +16,8 @@ class TestParallel(unittest.TestCase):
             return x + 1
 
         numbers = range(1000)
-
-        print(datetime.now())
-
         squared_numbers = map(_square, numbers)
-
-        print(datetime.now())
-
         results = parallel_map(_square, numbers, 500)
-
-        print(datetime.now())
-
         self.assertTrue(results == squared_numbers)
 
 if __name__ == '__main__':
