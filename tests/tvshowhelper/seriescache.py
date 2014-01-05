dee15935711732d9ed5445d4cdc9596208d24d5b
Michael Bang <michael@katnegermis.dk>
2014-01-05 15:49:55 +0100

    Add comments. Implement and add tests to seriescache. Implemented nextepisode, markwatched.

--- tests/tvshowhelper/seriescache.py
+++ tests/tvshowhelper/seriescache.py
@@ -16,21 +16,31 @@ from tvshowhelper.classes.episode import Episode
 class TestSeriesCache(unittest.TestCase):
 
     def setUp(self):
+        """ Create a test show and add 10 test episodes to it.
+
+        """
+        # Create test show.
         self.showname = 'The Test Show'
+        self.show = Show(name=self.showname)
+        seriescache._storeshow(self.show)
+
+        self.seasonnumber = 1
+        # Create test episodes.
+        # Every second episode, starting from episode 0, is unwatched.
         self.episodes = []
         for epnum in range(10):
             ep = Episode(number=epnum,
                          name="Episode " + str(epnum),
                          airdate=date.today(),
                          description="Description of episode {ep} of {name}".format(ep=epnum, name=self.showname),
-                         watched=True,
-                         seasonnumber=1,
+                         watched=epnum % 2 == 1,  # Mark every second as unwatched
+                         seasonnumber=self.seasonnumber,
                          showname=self.showname)
             self.episodes.append(ep)
-        seriescache._storeepisodes(self.episodes)
+        seriescache._storeepisodes(self.showname, self.episodes)
 
     def tearDown(self):
-        """ Delete test show and episodes when done testing
+        """ Delete test show and its' episodes.
 
         """
         db = seriescache._getdbcon()
@@ -44,27 +54,134 @@ class TestSeriesCache(unittest.TestCase):
               AND watched = ?
               AND season_number = ?
             """
+        # Delete episodes.
         episodes = map(lambda e: (e.number, e.name, e.airdate, e.description,
                                   e.watched, e.seasonnumber),
                        self.episodes)
         c.executemany(sql, episodes)
 
+        # Delete show.
         c.execute("DELETE FROM shows WHERE name = ?", (self.showname,))
+
         db.commit()
         db.close()
 
     def test_getepisode(self):
-        for epnum in range(10):
-            episode = seriescache.getepisode(showname=self.showname, seasonnum=1, episodenum=epnum)
-            self.assertTrue(episode.seasonnumber == 1 and episode.number == epnum and
-                            episode.showname == self.showname)
+        # We can find all episodes of the show we created.
+        for epnum in range(len(self.episodes)):
+            episode = seriescache.getepisode(showname=self.showname, seasonnum=1,
+                                             episodenum=epnum)
+            self.assertEquals(episode.seasonnumber, self.seasonnumber)
+            self.assertEquals(episode.number, epnum)
+            self.assertEquals(episode.showname, self.showname)
 
-        episode = seriescache.getepisode(showname="no such show", seasonnum=1, episodenum=epnum)
+        # We can't find an episode that doesn't exist.
+        nonexistentepisode = seriescache.getepisode(showname=self.showname,
+                                                    seasonnum=self.seasonnumber,
+                                                    episodenum=len(self.episodes))
+        self.assertIsNone(nonexistentepisode)
+
+        # We can't find an episode in a show that doesn't exist.
+        nonexistentshow = seriescache.getepisode(showname="no such show",
+                                                 seasonnum=self.seasonnumber,
+                                                 episodenum=epnum)
+        self.assertIsNone(nonexistentshow)
 
     def test_showexists(self):
+        # We can find a show that exists.
         self.assertTrue(seriescache.showexists(self.showname))
+
+        # We can't find a show that doesn't exist.
         self.assertFalse(seriescache.showexists('nonexistent show title'))
 
+        # Show names are case sensitive.
+        self.assertFalse(seriescache.showexists(self.showname.lower()))
+        self.assertFalse(seriescache.showexists(self.showname.upper()))
+
+    def test_episodeexists(self):
+        # We can find show that exists.
+        self.assertTrue(seriescache.episodeexists(self.showname,
+                                                  seasonnum=self.seasonnumber,
+                                                  episodenum=0))
+        # We can't find episode that doesn't exist.
+        self.assertFalse(seriescache.episodeexists(self.showname,
+                                                   seasonnum=self.seasonnumber,
+                                                   episodenum=999))
+
+        # We can't find episode in season that doesn't exist.
+        self.assertFalse(seriescache.episodeexists(self.showname,
+                                                   seasonnum=self.seasonnumber + 1,
+                                                   episodenum=0))
+
+        # We can't find episode from show that doesn't exist.
+        self.assertFalse(seriescache.episodeexists("nonexistent show",
+                                                   seasonnum=self.seasonnumber,
+                                                   episodenum=999))
+
+    def test_getnextepisode(self):
+        # We find first unwatched episode (Episode 0)
+        episode = seriescache.getnextepisode(self.showname)
+        self.assertEquals(episode.seasonnumber, self.seasonnumber)
+        self.assertEquals(episode.number, 0)
+
+        # We find the same episode since it hasn't been marked watched (Episode 0)
+        for __ in range(100):
+            episode = seriescache.getnextepisode(self.showname)
+            self.assertEquals(episode.seasonnumber, self.seasonnumber)
+            self.assertEquals(episode.number, 0)
+
+        # We find next unwatched episode (Episode 2), since first has been marked watched.
+        seriescache.markwatched(episode, markprevious=False, watched=True)
+        episode = seriescache.getnextepisode(self.showname)
+        self.assertEquals(episode.seasonnumber, self.seasonnumber)
+        self.assertEquals(episode.number, 2)
+
+    def test_markwatched_single(self):
+        epnum = 2
+        # Episode is unwatched
+        episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                         episodenum=epnum)
+        self.assertFalse(episode.watched)
+
+        # We can mark episode watched
+        seriescache.markwatched(episode, markprevious=False, watched=True)
+
+        # Episode has been marked watched
+        episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                         episodenum=epnum)
+        self.assertTrue(episode.watched)
+
+        # We can mark episode unwatched
+        seriescache.markwatched(episode, markprevious=False, watched=False)
+
+        # Episode has been marked unwatched
+        episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                         episodenum=epnum)
+        self.assertFalse(episode.watched)
+
+    def test_markwatched_mark_previous(self):
+        half = (len(self.episodes) - 1) // 2
+        # Mark all episodes watched
+        episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                         episodenum=len(self.episodes) - 1)
+        seriescache.markwatched(episode, markprevious=True, watched=True)
+
+        # Mark first half of episodes unwatched
+        episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                         episodenum=half)
+        seriescache.markwatched(episode, markprevious=True, watched=False)
+
+        # First half is unwatched
+        for epnum in range(half + 1):
+            episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                             episodenum=epnum)
+            self.assertFalse(episode.watched)
+
+        # Second half is watched
+        for epnum in range(half + 1, len(self.episodes)):
+            episode = seriescache.getepisode(self.showname, seasonnum=self.seasonnumber,
+                                             episodenum=epnum)
+            self.assertTrue(episode.watched)
 
 if __name__ == '__main__':
     unittest.main()
--- tvshowhelper/seriescache.py
+++ tvshowhelper/seriescache.py
@@ -41,15 +41,25 @@ def _getdbcon():
 
 
 def getepisode(showname, seasonnum, episodenum, update=False):
+    """ Get a specific episode from a show. Possibly update the show cache.
+
+    """
+    # Check if show exists in database, or try to find it on IMDB.
     if not showexists(showname):
+        print("'{show}' not found in database!".format(show=showname))
         show = imdb.getshow(showname)
+        if show is None:
+            # Show not found on imdb.
+            return None
         _storeshow(show)
     if update:
         updateshow(showname)
+    # Show exists. Try to find requested episode.
     sql = """
         SELECT e.number, e.name, e.airdate, e.description, e.watched, e.season_number
         FROM shows AS s, episodes AS e
         WHERE s.name = ?
+          AND s.id = e.show_id
           AND e.season_number = ?
           AND e.number = ?;
         """
@@ -58,20 +68,60 @@ def getepisode(showname, seasonnum, episodenum, update=False):
     c.execute(sql, (showname, seasonnum, episodenum))
     rows = c.fetchall()
     if rows == []:
-        # Episode not found.
+        # Episode wasn't found.
+        print("{show} S{s}E{e} not found in database!".format(show=showname,
+                                                              s=seasonnum,
+                                                              e=episodenum))
         return None
+    # Episode found.
     assert len(rows) == 1, "Duplicate episode in database"
     db.close()
     return _rowtoepisode(showname, rows[0])
 
 
 def getnextepisode(showname):
+    """ Get the first unwatched episode of a show.
+
+    """
     db = _getdbcon()
+    c = db.cursor()
+    # Find the first unwatched show by using SQL ordering
+    sql = """
+        SELECT e.number, e.name, e.airdate, e.description, e.watched, e.season_number
+        FROM shows AS s, episodes AS e
+        WHERE s.name = ?
+          AND s.id = e.show_id
+          AND e.watched = 0
+        ORDER BY e.number, e.season_number ASC
+        """
+    c.execute(sql, (showname,))
+    row = c.fetchone()
     db.close()
+    return _rowtoepisode(showname, row)
 
 
-def markwatched(showname, episode, markprevious=False, watched=True):
+def markwatched(episode, markprevious=False, watched=True):
+    """ Set the watched-attribute of one and/or all previous episodes of show.
+
+    """
+    if not episodeexists(showname=episode.showname,
+                         seasonnum=episode.seasonnumber,
+                         episodenum=episode.number):
+        print "{ep} doesn't exist in database!".format(episode.getprettyname())
+        return
+    # Episode exists in DB.
     db = _getdbcon()
+    c = db.cursor()
+    sql = """
+        UPDATE episodes
+           SET watched = ?
+           WHERE season_number {eq} ?
+             AND number {eq} ?
+             AND show_id = ?
+        """.format(eq="<=" if markprevious else "=")
+    c.execute(sql, (watched, episode.seasonnumber, episode.number,
+                    _getshowid(episode.showname)))
+    db.commit()
     db.close()
 
 
@@ -82,6 +132,9 @@ def updateshow(showname):
 
 
 def showexists(showname):
+    """ Tell if a show already exists in the database.
+
+    """
     db = _getdbcon()
     c = db.cursor()
     sql = """
@@ -96,21 +149,64 @@ def showexists(showname):
     return res == 1
 
 
+def _getshowid(showname):
+    db = _getdbcon()
+    c = db.cursor()
+    sql = """
+        SELECT *
+        FROM shows
+        WHERE name = ?;
+        """
+    c.execute(sql, (showname,))
+    rows = c.fetchall()
+    res = len(rows) if rows != [] else 0
+    assert res in (0, 1), "Duplicate shows in database!"
+    return rows[0][0]
+
+
+def episodeexists(showname, seasonnum, episodenum):
+    db = _getdbcon()
+    c = db.cursor()
+    sql = """
+        SELECT *
+        FROM shows AS s, episodes AS e
+        WHERE s.name = ?
+          AND s.id = e.show_id
+          AND e.season_number = ?
+          AND e.number = ?
+        """
+    c.execute(sql, (showname, seasonnum, episodenum))
+    rows = c.fetchall()
+    assert len(rows) in (0, 1), "Duplicate episodes in database!"
+    return len(rows) == 1
+
+
 def _rowtoshow(row):
     pass
 
 
 def _rowtoepisode(showname, row):
+    """ Convert a row from sqlite to an instance of Episode.
+    Assumes that 'row' is a listy type with episode information in the
+    following order:
+    [number, name, airdate, description, watched, seasonnumber]
+
+    """
     return Episode(number=row[0],
                    name=row[1],
                    airdate=datetime.strptime(row[2], SQL_DATE_FORMAT).date(),
                    description=row[3],
-                   watched=row[4],
+                   watched=row[4] == 1,
                    seasonnumber=row[5],
                    showname=showname)
 
 
 def _storeshow(show):
+    """ Store a show (instance of Show).
+
+    """
+    if show is None:
+        return
     sql = """
         INSERT INTO shows (name)
         VALUES (?);
@@ -121,19 +217,28 @@ def _storeshow(show):
     db.commit()
     db.close()
     for season in show.seasons:
-        _storeepisodes(season.episodes)
+        _storeepisodes(show.name, season.episodes)
 
 
-def _storeepisodes(episodes):
-    showname = episodes[0].showname
+def _storeepisodes(showname, episodes):
+    """ Store a listy of episodes in the database (instances of Episode).
+
+    """
+    if not all(map(lambda e: e.showname == showname, episodes)):
+        print("Not all episodes belong to the same show! Will not store the episodes.")
+        return
     if not showexists(showname):
         show = imdb.getshow(showname)
+        if show is None:
+            print "Couldn't find show on IMDB."
+            return
         _storeshow(show)
+    showid = _getshowid(showname)
     sql = """
-        INSERT INTO episodes (number, name, airdate, description, watched, season_number)
-        VALUES (?, ?, ?, ?, ?, ?);
+        INSERT INTO episodes (show_id, number, name, airdate, description, watched, season_number)
+        VALUES (?, ?, ?, ?, ?, ?, ?);
         """
-    episodetuples = map(lambda e: (e.number, e.name, e.airdate, e.description,
+    episodetuples = map(lambda e: (showid, e.number, e.name, e.airdate, e.description,
                                    e.watched, e.seasonnumber),
                         episodes)
     db = _getdbcon()
